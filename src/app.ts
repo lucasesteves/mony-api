d44d4f55ede94b95a06968b6d1d2a728399dd42f
lucasesteves <aderne20@gmail.com>
2021-09-19 17:40:47 -0300

    change port listen

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,26 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import express from 'express'
-import { router } from './routes'
-import cors from 'cors';
-import endpoint from './config/config';
-import mongoose from 'mongoose';
+import express from "express";
+import { router } from "./routes";
+import cors from "cors";
+import endpoint from "./config/config";
+import mongoose from "mongoose";
 
-const app = express()
+const app = express();
 app.use(cors());
 
-mongoose.connect(endpoint.DB_URL,{ useNewUrlParser: true })
-.then(()=>app.listen(endpoint.PORT,()=>{console.log('Server running at port '+endpoint.PORT)}))
-.catch((err) => {
-    console.error(err)
+mongoose
+  .connect(endpoint.DB_URL, { useNewUrlParser: true })
+  .then(() =>
+    app.listen(process.env.PORT || endpoint.PORT, () => {
+      console.log("Server running at port " + endpoint.PORT);
+    })
+  )
+  .catch((err) => {
+    console.error(err);
     process.exit(1);
-})
-    
-app.use(express.json())
-app.use(router)
+  });
+
+app.use(express.json());
+app.use(router);
