5e5af12e1e2d487194da40726ff673ad3f86d45f
lucasesteves <aderne20@gmail.com>
2021-09-19 17:24:43 -0300

    add route test

--- src/routes.ts
+++ src/routes.ts
@@ -1,26 +1,27 @@
-import { Router } from "express"
-import UserController from './controllers/UserController';
-import GainController from './controllers/WinController';
-import LossController from './controllers/LoseController';
-import DashboardController from './controllers/DashboardController';
-import { checkJwt } from './middleware/authentication';
+import { Router, Response } from "express";
+import UserController from "./controllers/UserController";
+import GainController from "./controllers/WinController";
+import LossController from "./controllers/LoseController";
+import DashboardController from "./controllers/DashboardController";
+import { checkJwt } from "./middleware/authentication";
 
-const router = Router()
+const router = Router();
 
-router.post('/login', UserController.login)
-router.post('/register', UserController.create)
-router.get('/user/:id', [checkJwt], UserController.getUser)
+router.get("/login", (res: Response) => res.send("ok"));
+router.post("/login", UserController.login);
+router.post("/register", UserController.create);
+router.get("/user/:id", [checkJwt], UserController.getUser);
 
-router.post('/dashboard', [checkJwt], DashboardController.getDifference)
+router.post("/dashboard", [checkJwt], DashboardController.getDifference);
 
-router.post('/gain/save', [checkJwt], GainController.save)
-router.delete('/gain/remove/:id', [checkJwt], GainController.remove)
-router.post('/gain/total', [checkJwt], GainController.sumOfWins)
-router.post('/gain', [checkJwt], GainController.getAllGain)
+router.post("/gain/save", [checkJwt], GainController.save);
+router.delete("/gain/remove/:id", [checkJwt], GainController.remove);
+router.post("/gain/total", [checkJwt], GainController.sumOfWins);
+router.post("/gain", [checkJwt], GainController.getAllGain);
 
-router.post('/loss/save', [checkJwt], LossController.save)
-router.delete('/loss/remove/:id', [checkJwt], LossController.remove)
-router.post('/loss/total', [checkJwt], LossController.sumOfLoss)
-router.post('/loss', [checkJwt], LossController.getAllLoss)
+router.post("/loss/save", [checkJwt], LossController.save);
+router.delete("/loss/remove/:id", [checkJwt], LossController.remove);
+router.post("/loss/total", [checkJwt], LossController.sumOfLoss);
+router.post("/loss", [checkJwt], LossController.getAllLoss);
 
-export { router }
+export { router };
